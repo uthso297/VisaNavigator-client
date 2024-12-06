5da7edfb569e855283515ff21b963e0aeb32833a
uthso297 <uthso119464@gmail.com>
2024-12-06 18:46:09 +0600

    completed enviroment variable for firebase

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCtG-6vfyO8ifknH4AnK2aAVw9CxSlBdTQ",
-  authDomain: "visa-navigator-portal-f4954.firebaseapp.com",
-  projectId: "visa-navigator-portal-f4954",
-  storageBucket: "visa-navigator-portal-f4954.firebasestorage.app",
-  messagingSenderId: "1016131715035",
-  appId: "1:1016131715035:web:8c89deabea67a80755b1fe"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
